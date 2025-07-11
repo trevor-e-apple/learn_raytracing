6731b6082d011265f3849ae792097ce50d47d13e
Trevor Apple <trevorapple@MacBook-Air.local>
2025-07-11 09:53:28 -0700

    viewport stuff

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,22 @@ mod vector;
 mod ray;
 
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = {
+        let height = (image_width as f64 / aspect_ratio) as i32;
+
+        if height > 1 {
+            height
+        } else {
+            1
+        }
+    };
+
+    // Viewport dimensions can be real valued
+    let viewport_height = 2.0;
+    // We don't reuse aspect ratio since the image_height is not real-valued and doesn't represent the *true* ratio
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
 
     println!("P3");
     println!("{} {} 255", image_width, image_height);
