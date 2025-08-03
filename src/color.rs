f507b45e07d3a80170e7912070e5c0e004ba9966
Trevor Apple <trappleco@gmail.com>
2025-08-03 07:31:46 -0700

    gamma correction

--- src/color.rs
+++ src/color.rs
@@ -2,10 +2,22 @@ pub use crate::vector::Vector3;
 
 pub use Vector3 as Color;
 
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        linear_component.sqrt()
+    } else {
+        0.0
+    }
+}
+
 pub fn write_color(color: &Color) {
-    let r = color.x.clamp(0.0, 0.999);
-    let g = color.y.clamp(0.0, 0.999);
-    let b = color.z.clamp(0.0, 0.999);
+    let r = linear_to_gamma(color.x);
+    let g = linear_to_gamma(color.y);
+    let b = linear_to_gamma(color.z);
+
+    let r = r.clamp(0.0, 0.999);
+    let g = g.clamp(0.0, 0.999);
+    let b = b.clamp(0.0, 0.999);
 
     let ir = (255.999 * r) as i64;
     let ig = (255.999 * g) as i64;
