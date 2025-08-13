d65c0f702c9a018999406014416801bf65bc61fa
Trevor Apple <trappleco@gmail.com>
2025-08-13 08:40:48 -0700

    restart for better practice

--- src/main.rs
+++ src/main.rs
@@ -1,135 +1,26 @@
-use std::rc::Rc;
-
-use crate::{
-    camera::Camera,
-    color::Color,
-    hittable::HittableList,
-    material::{Dielectric, Lambertian, Metal},
-    sphere::Sphere,
-    vector::Vector3,
-};
-
-mod camera;
-mod color;
-mod hittable;
-mod material;
-mod math;
-mod random_vector;
-mod ray;
-mod sphere;
 mod vector;
-mod vector_raytrace;
 
 fn main() {
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
+    let image_width = 256;
+    let image_height = 256;
 
-    // Camera
-    let mut camera = Camera::new(
-        20.0,
-        aspect_ratio,
-        image_width,
-        100,
-        50,
-        Vector3 {
-            x: -2.0,
-            y: 2.0,
-            z: 1.0,
-        },
-        Vector3 {
-            x: 0.0,
-            y: 0.0,
-            z: -1.0,
-        },
-        Vector3 {
-            x: 0.0,
-            y: 1.0,
-            z: 0.0,
-        },
-        3.4,
-        10.0
-    );
+    println!("P3");
+    println!("{} {}", image_width, image_height);
+    println!("255");
 
-    // Materials
-    let material_ground = Rc::new(Lambertian {
-        albedo: Color {
-            x: 0.8,
-            y: 0.8,
-            z: 0.0,
-        },
-    });
-    let material_center = Rc::new(Lambertian {
-        albedo: Color {
-            x: 0.1,
-            y: 0.2,
-            z: 0.5,
-        },
-    });
-    let material_left = Rc::new(Dielectric {
-        refraction_index: 1.5,
-    });
-    let material_bubble = Rc::new(Dielectric {
-        refraction_index: 1.0 / 1.5,
-    });
-    let material_right = Rc::new(Metal {
-        albedo: Color {
-            x: 0.8,
-            y: 0.6,
-            z: 0.2,
-        },
-        fuzz: 1.0,
-    });
+    for x in 0..image_width {
+        for y in 0..image_height {
+            let r: i32 = {
+                let r = (x as f64) / (image_width as f64);
+                (255.99 * r) as i32
+            };
+            let g: i32 = {
+                let g = (y as f64) / (image_height as f64);
+                (255.99 * g) as i32
+            };
+            let b: i32 = 0;
 
-    // World
-    let world = HittableList {
-        objects: vec![
-            Rc::new(Sphere {
-                center: Vector3 {
-                    x: 0.0,
-                    y: -100.5,
-                    z: -1.0,
-                },
-                radius: 100.0,
-                mat: material_ground,
-            }),
-            Rc::new(Sphere {
-                center: Vector3 {
-                    x: 0.0,
-                    y: 0.0,
-                    z: -1.2,
-                },
-                radius: 0.5,
-                mat: material_center,
-            }),
-            Rc::new(Sphere {
-                center: Vector3 {
-                    x: -1.0,
-                    y: 0.0,
-                    z: -1.0,
-                },
-                radius: 0.5,
-                mat: material_left,
-            }),
-            Rc::new(Sphere {
-                center: Vector3 {
-                    x: -1.0,
-                    y: 0.0,
-                    z: -1.0,
-                },
-                radius: 0.4,
-                mat: material_bubble,
-            }),
-            Rc::new(Sphere {
-                center: Vector3 {
-                    x: 1.0,
-                    y: 0.0,
-                    z: -1.0,
-                },
-                radius: 0.5,
-                mat: material_right,
-            }),
-        ],
-    };
-
-    camera.render(&world);
+            println!("{} {} {}", r, g, b);
+        }
+    }
 }
